2476a05dba0e9666c38559a1d471bee9415c6819
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-03-12 23:21:28 -0400

    Configurando background do game

--- PraCima/Controllers/GameViewController.swift
+++ PraCima/Controllers/GameViewController.swift
@@ -13,30 +13,18 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let view = self.view as? SKView else  {
+                return
+        }
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+        scene.scaleMode = .aspectFill
+        
+        view.ignoresSiblingOrder = true
+        view.showsFPS = true
+        view.showsNodeCount = true
+        view.showsPhysics = true
+        view.presentScene(scene)
     }
 
     override var prefersStatusBarHidden: Bool {
